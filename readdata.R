b4a66f0f58bc099a095069a92a5095239332742a
Camilo Strange <camilor@gmail.com>
2019-11-02 07:52:11 -0400

    comments

--- readdata.R
+++ readdata.R
@@ -1,13 +1,19 @@
 library(tidyverse)
 library(purrr)
+library(rmarkdown)
 
+
+#read the data
 datapath <- "./data/"
 marvel <- "marvel-wikia-data.csv"
 marveldata <- paste(datapath, marvel, sep="")
 marvelcsv <- read_csv(marveldata)
+
+#convert these things to factor
 columns <- c("ALIGN", "EYE", "HAIR", "SEX", "GSM", "ALIVE")
 
 #next step: to add a function that goes through the columns list and makes the  transformation into factor ftromusing a function, using the example below:
+# purrr:map() something something
 
 marvelcsv$ALIGN <- marvelcsv$ALIGN  %>% as_factor()
 
